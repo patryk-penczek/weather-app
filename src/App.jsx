667d4fcf24c78ccfc58f92a8ec9b152e4c00022c
Desparek <desparvek@gmail.com>
2023-01-22 22:51:57 +0100

    input ref and keypress functionality

--- src/App.jsx
+++ src/App.jsx
@@ -11,21 +11,12 @@ function App() {
 
   useEffect(() => {
     fetch(`http://api.weatherapi.com/v1/current.json?key=8afc0e8503da447387c193327232201&q=${city}`)
-    .then((response) => {
-      if(response.status === 200){
-        response.json();
-      } else {
-        throw new Error("Something went wrong");
-      }
-    })
-    .then((data) => {
-      console.log(data);
-    })
+    .then(response => response.json())
+    .then(data => console.log(data))
   }, [city]);
   return (
     <div className="h-screen flex flex-col justify-center items-center">
-      <SearchBar />
-      <button onClick={() => setCity("Katowice")}>XXXXXXXXXXXX</button>
+      <SearchBar setCity={setCity} />
       <Container />
       <Footer />
     </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,16 @@
-const SearchBar= () => {
+import { useRef } from "react";
+
+const SearchBar = ({setCity}) => {
+    const searchRef = useRef();
+    const handlePressEnter = (event) => {
+        if (event.key === 'Enter'){
+            setCity(searchRef.current.value);
+        }
+    }
     return (
         <>
             <label className="sr-only" htmlFor="search-bar">Enter a city...</label>
-            <input className="border-2 border-slate-900 p-1 rounded-xl mb-4" id="search-bar" type="text" placeholder="Enter a city..." />
+            <input onKeyDown={handlePressEnter} className="border-2 border-slate-900 p-1 rounded-xl mb-4" ref={searchRef} id="search-bar" type="text" placeholder="Enter a city..." />
         </>
     )
 }
